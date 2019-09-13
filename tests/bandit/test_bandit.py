67df4efc669e7faebee11bcb1d7955e9f3d39071
DanielMorton <dacmorton@gmail.com>
2019-09-12 23:12:35 -0400

    Fix test

--- tests/bandit/test_bandit.py
+++ tests/bandit/test_bandit.py
@@ -11,14 +11,14 @@ class TestBandit(TestCase):
         assert bandit.arms == 5
         assert bandit.best_arm == 2
         assert bandit.max_reward == 0.95
-        assert bandit.means == np.array([0.86, 0.46, 0.95, 0.58, 0.24])
+        assert np.array_equal(bandit.means, np.array([0.86, 0.46, 0.95, 0.58, 0.24]))
 
     def test_other_bandit(self):
         bandit = Bandit(np.array([0.18, 0.91, 0.41, 0.93, 0.61, 0.43, 0.35, 0.9, 0.67]))
         assert bandit.arms == 9
         assert bandit.best_arm == 3
         assert bandit.max_reward == 0.93
-        assert bandit.means == np.array([0.18, 0.91, 0.41, 0.93, 0.61, 0.43, 0.35, 0.9, 0.67])
+        assert np.array_equal(bandit.means, np.array([0.18, 0.91, 0.41, 0.93, 0.61, 0.43, 0.35, 0.9, 0.67]))
 
     def test_empty_bandit(self):
         self.assertRaises(AssertionError, Bandit, np.array([]))
