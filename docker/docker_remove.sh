aaead381630837fc45d496850536e111e69e4ee1
DanielMorton <dacmorton@gmail.com>
2020-01-22 10:49:06 -0500

    Add docker commands

--- docker/docker_remove.sh
+++ docker/docker_remove.sh
@@ -2,8 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker system prune -f
-
 source ${DIR}/docker_env.sh
 
-docker build ${DIR}/.. -f ${DIR}/Dockerfile -t ${DOCKER_IMAGE_NAME}
+docker rmi -f ${DOCKER_IMAGE_NAME}
---
+++ docker/docker_run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ${DIR}/docker_env.sh
+
+while :;
+do
+    case $1 in
+        -r|--runs)
+            RUNS="$2"
+            shift
+            ;;
+        -l|--length)
+            LENGTH="$2"
+            shift
+            ;;
+        -e|--epsilon)
+            TYPE="-e"
+            shift
+            ;;
+        -g|--greedy)
+            TYPE="-g"
+            shift
+            ;;
+        *)  break
+    esac
+
+    shift
+done
+
+docker run ${DOCKER_IMAGE_NAME} -l $LENGTH -r $RUNS $TYPE
